7ace76de62cdd7ee7d53ce1ac96aece7012996f1
SAMUEL GLADLY <samuelrosy0210@gmail.com>
2022-11-04 19:14:55 +0530

    Delete OIBGRIP-main directory

--- OIBGRIP-main/js/tbp_script.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed(".typing",{
-    strings: ["An Inspiration", "Helper", "Feeds People", "Theosophist","Nobel price for peace", "Poor People's God", "Reformer",],
-    typeSpeed:100,
-    backSpeed:80,
-    loop:true
-});
